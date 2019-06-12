ecba0f04a4b86b8e3ba334fa685969f9c63fecc4
Your Name <you@example.com>
2019-06-12 18:04:31 +0800

    update

--- spyder.py
+++ spyder.py
@@ -3,9 +3,9 @@ import requests
 import urllib
 from bs4 import BeautifulSoup
 
-url = 'http://www.sohu.com/a/314432722_559488?scm=1019.e000a.v1.all&spm=smpc.csrpage.news-list.2.1560319936429k7bcgdz'
+url = 'https://www.sohu.com/a/221728304_541698'
 
-image_dir = './lusuo/'
+image_dir = './leinuoa/'
 if not os.path.exists(image_dir):
     os.mkdir(image_dir)
 
