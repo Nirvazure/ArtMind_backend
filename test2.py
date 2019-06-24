aa815bde2897ba8f42407b5856bf1269c40af5c5
Your Name <you@example.com>
2019-06-24 15:11:54 +0800

    update

--- test2.py
+++
@@ -1,9 +0,0 @@
-import torch
-from net.ArtNet import ArtNet
-from config import num_classes, batch_size, learning_rate, test_path, saved_model_path, img_size
-
-# Device configuration
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-# model = InceptionV4(num_classes).to(device)
-model = ArtNet(num_classes).to(device)
-model.load_state_dict(torch.load(saved_model_path))
