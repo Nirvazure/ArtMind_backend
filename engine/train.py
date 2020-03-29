49fe48fdb15ea01d87702473390b225aa03b4bb6
Nirvazure <nirvazure@github.com>
2020-03-29 10:24:35 +0800

    update

--- engine/train.py
+++ engine/train.py
@@ -26,11 +26,11 @@ train_loader = torch.utils.data.DataLoader(
     trainset, batch_size=20, shuffle=True)
 
 # 查看训练数据集样子的代码
-# for i, (batch_x, batch_y) in enumerate(train_loader):
-#     print(i, batch_x.size(), batch_y.size())
-#     show_batch(batch_x)
-#     plt.axis('off')
-#     plt.show()
+for i, (batch_x, batch_y) in enumerate(train_loader):
+    print(i, batch_x.size(), batch_y.size())
+    show_batch(batch_x)
+    plt.axis('off')
+    plt.show()
 
 model = ArtNet(num_classes).to(device)
 # model = InceptionV4(num_classes).to(device)
--- flask/config.py
+++ flask/config.py
@@ -1,7 +1,7 @@
 import os
 from app import app
 
-basedir=os.path.abspath(os.path.dirname(__file__))
-UPLOAD_FOLDER='upload'
+basedir=os.path.abspath(os.path.dirname(__file__))      #配置基地址
+UPLOAD_FOLDER='upload'                                  #配置上传文件存储位置
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS=set(['png','jpg','JPG','PNG','gif','GIF'])
+ALLOWED_EXTENSIONS=set(['png','jpg','JPG','PNG','gif','GIF'])   #限制上传图像文件格式
